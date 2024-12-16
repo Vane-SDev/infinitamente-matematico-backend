973c6dd7e896ed94185cd8e0f163fde9d160e69a
Vanesa Soria <106666059+Vane-SDev@users.noreply.github.com>
2024-12-16 11:30:32 -0300

    Update tests-email.js

--- tests-email.js
+++ tests-email.js
@@ -1,11 +1,11 @@
-const sendEmail = require('./utils/email'); // Ajusta la ruta según tu estructura
+const sendEmail = require('./utils/email'); 
+
 
 const testEmail = async () => {
     try {
         await sendEmail(
-            'infinitamentematematico@gmail.com', // Cambia por tu correo o el de prueba
-            'Prueba de correo',
-            'Este es un correo de prueba desde la app Infinitamente Matemático.'
+            
+           
         );
         console.log('Correo enviado correctamente.');
     } catch (error) {
